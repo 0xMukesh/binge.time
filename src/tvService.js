ba5dc65293c45065eaa914ee6c5f888ea2b9b577
Kira <mukeshreddy.9.new@gmail.com>
2022-01-22 18:01:37 +0530

    fix(tvService.js): cleaned up the comments

--- src/tvService.js
+++ src/tvService.js
@@ -1,11 +1,10 @@
-//format: /search/shows?q=:query
 
 const searchUrl = "https://api.tvmaze.com/search/shows?q=";
 
 const showUrl = "";
 
 function to_url_format(query) {
-    let lowercase = query.toLowerCase().trim(); //to lowercase and eliminating trailing whitespace
+    let lowercase = query.toLowerCase().trim();
     return lowercase.split(' ').join('-');
 }
 
@@ -21,7 +20,6 @@ export async function searchShow(query) {
 const episodesPrefix = "https://api.tvmaze.com/shows/"
 const episodesSuffix = "/episodes"
 
-//With data from http://api.tvmaze.com/shows/{show_id}/episodes
 export async function showInfo(ID) {
     var idString = ID + ''
     let url = episodesPrefix + ID + episodesSuffix + ``;
@@ -31,10 +29,6 @@ export async function showInfo(ID) {
     return result;
 }
 
-
-
-//Returns minutesWatched, minutesLeft
-//Untested :D
 function minutes_watched_minutes_left(startSeason, startEpisode, endSeason, endEpisode, showEpisodes) {
     startSeason = parseInt(startSeason);
     startEpisode = parseInt(startEpisode);
@@ -44,7 +38,6 @@ function minutes_watched_minutes_left(startSeason, startEpisode, endSeason, endE
     endEpisode = endEpisode - 1;
     let minutesWatched = 0;
     let minutesLeft = 0;
-    //Minutes watched
     for (let season = 1; season <= startSeason; season++) {
         if (season === startSeason) {
             for (let episode = 0; episode < startEpisode; episode++) {
@@ -57,7 +50,6 @@ function minutes_watched_minutes_left(startSeason, startEpisode, endSeason, endE
             }
         }
     }
-    //Minutes Left
     for (let season = startSeason; season <= endSeason; season++) {
         if (startSeason === endSeason) {
             for (let episode = startEpisode; episode <= endEpisode; episode++) {
@@ -83,15 +75,7 @@ function percentage_watched(startSeason, startEpisode, endSeason, endEpisode, sh
     return stats[0] / stats[1];
 }
 
-//I have an idea of this data structure
-//{SeasonNumber: [[EpisodeTitle, Runtime]...]}} //Episode title might not be useful,
-//But I figure the episode number is implicity from the list
-//Example:
-//{1: [[Ep1Name, 47], [Ep2Name, 51], [Ep3Name: 46]...], 2: [youGetTheIdea]..., ...}
-//I considered doing something like 
-//{1: {Ep1Name: 47, Ep2Name: 51, Ep3Name: 46...}, 2: {youGetTheIdea}...}
-//But I was concered the map indexes don't work the same way, and then the episode
-// number wouldn't be implicit from the data
+
 export function to_episodes_map(data) {
     let results = {};
     for (let i = 0; i < data.length; i++) {
@@ -165,41 +149,6 @@ export function episodesPerDay(startSeason, startEpisode, endSeason, endEpisode,
     const utc1 = Date.UTC(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate());
     const utc2 = Date.UTC(end.getFullYear(), end.getMonth(), end.getDate());
     const dateDiff = Math.floor((utc2 - utc1) / _MS_PER_DAY) + 1;
-    //alert("Time Frame: " + (Math.ceil((end.getTime() - tomorrow.getTime()) / 86400000) + 1));
-    //alert("Time Frame: " + getTimeDiff(endDate));
-    // let timeFrame = Math.floor((end.getTime() - tomorrow.getTime()) / 86400000);
-    // let rate = totalEpisodes / timeFrame
     let rate = totalEpisodes / dateDiff;
     return rate;
 }
-
-/*
-function getTimeDiff(endDate) {
-    let today = new Date();
-    let tmw = addDays(today, 1);
-    let tomorrowNew = new Date(tmw.getYear(), tmw.getMonth(), tmw.getDate());
-    let endDateNew = new Date(endDate.getYear(), endDate.getMonth(), endDate.getDate());
-    let millisecondsPerDay = 1000 * 60 * 60 * 24;
-    let millisBetween = endDateNew.getTime() - tomorrowNew.getTime();
-    let days = millisBetween / millisecondsPerDay;
-    return Math.floor(days) + 1;
-}*/
-
-// const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-// // a and b are javascript Date objects
-// function dateDiffInDays(a, b) {
-//   // Discard the time and time-zone information.
-
-
-
-// }
-
-// // test it
-// const a = new Date("2017-01-01"),
-//     b = new Date("2017-07-25"),
-//     difference = dateDiffInDays(a, b);
-
-
-
-
