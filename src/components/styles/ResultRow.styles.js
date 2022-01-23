01452470a02623a1fb9a94fc2638d0a57b37ca34
Atharv-777 <atharvbidwe11@gmail.com>
2022-01-23 16:22:04 +0530

    Added responsiveness

--- src/components/styles/ResultRow.styles.js
+++ src/components/styles/ResultRow.styles.js
@@ -7,16 +7,24 @@ export const ResultContainer = styled.div`
   height: 50px;
   padding: 5px;
   display: flex;
-  width: 40vw;
+  width: 40%;
   justify-content: space-between;
   border-radius: 5px;
   cursor: pointer;
+  scrollbar-width: 480px;
   &:hover {
     background-color: ${primary};
     transition: 0.3s background-color;
   }
   background-color: ${(props) => (props.selected ? props.selected : 'none')};
   transition: 0.3s background-color;
+
+  @media screen and (max-width: 650px){
+    width:70%
+  }
+  @media screen and (max-width: 480px){
+    width:90%
+  }
 `;
 
 export const ResultImage = styled.img`
--- src/pages/styles/Step1.styles.jsx
+++ src/pages/styles/Step1.styles.jsx
@@ -11,30 +11,62 @@ export const Container = styled.div`
 export const SearchBarDiv = styled.div`
   display: flex;
   flex-direction: row;
-  width: 41%;
+  width: 60%;
+  
+  @media screen and (max-width: 700px){
+    width: 80%;
+    display:block;
+    text-align:center;
+  }
 `;
 
 export const SearchBar = styled.input`
   border-radius: 5px;
   width: 80%;
   padding: 0px 15px;
+
+  @media screen and (max-width: 700px){
+    padding:15px 5%;
+    margin-bottom:20px;
+  }
+
+  @media screen and (max-width: 480px){
+    width:95%;
+    outline:none;
+  }
 `;
 
 export const Button = styled.button`
   border-radius: 5px;
   height: 50px;
-  width: 20%;
+  width: 100px;
   background-color: ${primary};
+
+
+  @media screen and (max-width: 700px, min-width:480px){
+  }
+  @media screen and (max-width: 480px){
+  }
 `;
 
 export const ResultsDiv = styled.div`
   display: flex;
   flex-direction: row;
   border-radius: 5px;
-  max-height: 310px;
+  max-height: auto;
   width: 100%;
   margin: 5vh auto;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+  scrollbar-width: 480px;
+
+  @media screen and (max-width: 650px){
+    width:80%
+    display:block;
+    text-align:center
+  }
+
+
+
 `;
